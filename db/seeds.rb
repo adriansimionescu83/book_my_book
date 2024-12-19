d1802a710136478ff8e67a804f7d32f609be48c4
Adrian Simionescu <simionescuadrian@yahoo.com>
2024-12-19 11:12:43 +0400

    [Feature] Some updates on the seeds to use other image generator than faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,108 +8,108 @@ require 'open-uri'
 
 
 # Creating Users
-users = User.create!(
-  [
-    {
-      first_name: 'Alice',
-      last_name: 'Johnson',
-      phone_number: '+971501234567',
-      address: '123 Palm Jumeirah, Dubai, UAE',
-      email: 'alice@example.com',
-      password: 'password', # Added password here
-    },
-    {
-      first_name: 'Bob',
-      last_name: 'Smith',
-      phone_number: '+971502345678',
-      address: '456 Downtown Dubai, UAE',
-      email: 'bob@example.com',
-      password: 'password', # Added password here
-    },
-    {
-      first_name: 'Carol',
-      last_name: 'Davis',
-      phone_number: '+971503456789',
-      address: '789 Dubai Marina, UAE',
-      email: 'carol@example.com',
-      password: 'password', # Added password here
-    }
-  ]
-)
+# users = User.create!(
+#   [
+#     {
+#       first_name: 'Alice',
+#       last_name: 'Johnson',
+#       phone_number: '+971501234567',
+#       address: '123 Palm Jumeirah, Dubai, UAE',
+#       email: 'alice@example.com',
+#       password: 'password', # Added password here
+#     },
+#     {
+#       first_name: 'Bob',
+#       last_name: 'Smith',
+#       phone_number: '+971502345678',
+#       address: '456 Downtown Dubai, UAE',
+#       email: 'bob@example.com',
+#       password: 'password', # Added password here
+#     },
+#     {
+#       first_name: 'Carol',
+#       last_name: 'Davis',
+#       phone_number: '+971503456789',
+#       address: '789 Dubai Marina, UAE',
+#       email: 'carol@example.com',
+#       password: 'password', # Added password here
+#     }
+#   ]
+# )
 
-# Creating Categories
-categories = Category.create!(
-  [
-    { name: 'Fiction' },
-    { name: 'Non-Fiction' },
-    { name: 'Science Fiction' },
-    { name: 'Fantasy' },
-    { name: 'Mystery' },
-    { name: 'Thriller' },
-    { name: 'Horror' },
-    { name: 'Romance' },
-    { name: 'Historical' },
-    { name: 'Biography' },
-    { name: 'Self-Help' },
-    { name: 'Health & Wellness' },
-    { name: 'Travel' },
-    { name: 'Cooking' },
-    { name: 'Art & Photography' },
-    { name: 'Science' },
-    { name: 'Philosophy' },
-    { name: 'Religion & Spirituality' },
-    { name: 'Poetry' },
-    { name: 'Drama' },
-    { name: 'Comics & Graphic Novels' },
-    { name: 'Children’s Books' },
-    { name: 'Young Adult' },
-    { name: 'Education' },
-    { name: 'Technology' },
-    { name: 'Business' },
-    { name: 'Politics' },
-    { name: 'Sports' },
-    { name: 'True Crime' },
-    { name: 'Humor' }
-  ]
-)
+# # Creating Categories
+# categories = Category.create!(
+#   [
+#     { name: 'Fiction' },
+#     { name: 'Non-Fiction' },
+#     { name: 'Science Fiction' },
+#     { name: 'Fantasy' },
+#     { name: 'Mystery' },
+#     { name: 'Thriller' },
+#     { name: 'Horror' },
+#     { name: 'Romance' },
+#     { name: 'Historical' },
+#     { name: 'Biography' },
+#     { name: 'Self-Help' },
+#     { name: 'Health & Wellness' },
+#     { name: 'Travel' },
+#     { name: 'Cooking' },
+#     { name: 'Art & Photography' },
+#     { name: 'Science' },
+#     { name: 'Philosophy' },
+#     { name: 'Religion & Spirituality' },
+#     { name: 'Poetry' },
+#     { name: 'Drama' },
+#     { name: 'Comics & Graphic Novels' },
+#     { name: 'Children’s Books' },
+#     { name: 'Young Adult' },
+#     { name: 'Education' },
+#     { name: 'Technology' },
+#     { name: 'Business' },
+#     { name: 'Politics' },
+#     { name: 'Sports' },
+#     { name: 'True Crime' },
+#     { name: 'Humor' }
+#   ]
+# )
 
-# # Create languages
-languages = [
-  "Mandarin Chinese",
-  "Spanish",
-  "English",
-  "Hindi",
-  "Arabic",
-  "Bengali",
-  "Portuguese",
-  "Russian",
-  "Japanese",
-  "Punjabi",
-  "German",
-  "Javanese",
-  "Wu (Shanghainese)",
-  "Turkish",
-  "Korean",
-  "French",
-  "Vietnamese",
-  "Telugu",
-  "Tamil",
-  "Urdu",
-  "Italian",
-  "Thai",
-  "Gujarati",
-  "Persian (Farsi)",
-  "Polish",
-  "Dutch",
-  "Romanian",
-  "Hungarian",
-  "Swedish",
-  "Ukrainian"
-]
+# # # Create languages
+# languages = [
+#   "Mandarin Chinese",
+#   "Spanish",
+#   "English",
+#   "Hindi",
+#   "Arabic",
+#   "Bengali",
+#   "Portuguese",
+#   "Russian",
+#   "Japanese",
+#   "Punjabi",
+#   "German",
+#   "Javanese",
+#   "Wu (Shanghainese)",
+#   "Turkish",
+#   "Korean",
+#   "French",
+#   "Vietnamese",
+#   "Telugu",
+#   "Tamil",
+#   "Urdu",
+#   "Italian",
+#   "Thai",
+#   "Gujarati",
+#   "Persian (Farsi)",
+#   "Polish",
+#   "Dutch",
+#   "Romanian",
+#   "Hungarian",
+#   "Swedish",
+#   "Ukrainian"
+# ]
 
-languages.each do |language|
-  Language.create(name: language)
-end
+# languages.each do |language|
+#   Language.create(name: language)
+# end
 
 # Destroy all books, chats and age groups if already created
 # Chat.destroy_all
@@ -130,7 +130,17 @@ Chat.destroy_all
 Book.destroy_all
 
 50.times do
-  file = URI.parse(Faker::LoremFlickr.image(size: "400x600", search_terms: ['book'])).open
+
+  image_url = "https://picsum.photos/400/600"
+  file = URI.parse(image_url).open
+
+  # begin
+  #   file = URI.parse(Faker::LoremFlickr.unique.image(size: "400x600", search_terms: ['book'])).open
+  # rescue OpenURI::HTTPError, Net::OpenTimeout => e
+  #   retry_count += 1
+  #   retry if retry_count < 3
+  #   puts "Failed to download image after 3 attempts: #{e.message}"
+  # end
 
   book = Book.create!(
     title: Faker::Book.title,
